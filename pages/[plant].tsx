0c4afbf51f77c95f9d60570c62c823dd1a632dd6
ElodieIdaLaps <59149456+ElodieLaps@users.noreply.github.com>
2021-02-16 10:43:28 +0100

    base tsx implemented

---
+++ pages/[plant].tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Plant() {
+   return (  
+      <Layout pageTitle="plant">
+         <h1>plant content</h1>
+      </Layout>
+   );
+}
+ 
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <>
+      <Layout pageTitle="page d'accueil">
+        <h1>home content</h1>
+      </Layout>
+    </>
+  )
+}
