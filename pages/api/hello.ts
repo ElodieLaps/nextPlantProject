ad6b1e9667b3a3b95ebe910868aae40ca5b4b3f6
ElodieIdaLaps <59149456+ElodieLaps@users.noreply.github.com>
2021-02-19 14:18:40 +0100

    ts getstaticprops for index refact

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export const key : string  = "E_3Wy2VspiI0ykCPxVnll0ha1x5XEBGbSavaOgaCwOI";
+import { NextApiRequest, NextApiResponse } from "next";
 
-/*export default (req, res) => {
+export const token : string  = "E_3Wy2VspiI0ykCPxVnll0ha1x5XEBGbSavaOgaCwOI";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
-}*/
+}
+
+/*(async () => {
+  const response = await axios.get(`https://trefle.io/api/v1/plants?token=${token}`);
+  const json = await response.json();
+  console.log(json);
+})();*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,48 @@
 import Layout from '../components/page/Layout'
 import Image from 'next/image'
+import { token } from './api/hello'
+import { InferGetStaticPropsType } from 'next'
 
-export default function Home() {
+export default function Home({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <>
-      <Layout pageTitle="page d'accueil">
-        <h1>home content</h1>
-        <Image 
-          src="/img/default_plant.jpg" 
-          alt="plante par défaut" 
-          width="200" 
-          height="200"
-          quality={60}
-          className="plant-picture rounded-2xl object-cover object-center m-1"
-        />
-      </Layout>
-    </>
+    <Layout pageTitle="page d'accueil">
+      <h1>home content</h1>
+      <Image
+        src="/img/default_plant.jpg"
+        alt="plante par défaut"
+        width="200"
+        height="200"
+        quality={60}
+        className="plant-picture rounded-2xl object-cover object-center m-1"
+      />
+      {console.log(page.data)}
+      {page.data.map((plant: Plant) =>
+        <div key={plant.id}>
+          {plant.common_name}
+        </div>
+      )}
+    </Layout>
   )
 }
+
+type Plant = {
+  id: number;
+  common_name: string;
+}
+
+type Page = {
+  data: Array<Plant>;
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://trefle.io/api/v1/plants?token=${token}`)
+  const page: Page = await res.json()
+
+  return {
+    props: {
+      page
+    },
+  }
+}
+
+
